ad3fa4ed66a5b8e150e40caac793ae3db8dadf83
派兹 <admin@blingwang.cn>
2018-10-20 15:01:59 +0800

    Add files via upload

---
+++ 2.4.0/init.lua
@@ -0,0 +1,13 @@
+appver="2.4"
+appcode="24000"
+packagename="com.blingwang.kedama"
+appname="毛玉线圈物语"
+user_permission={
+  [1]	= "ACCESS_COARSE_LOCATION" ;
+  [2]	= "ACCESS_FINE_LOCATION" ;
+  [3]	= "ACCESS_NETWORK_STATE" ;
+  [4]	= "ACCESS_WIFI_STATE" ;
+  [5]	= "INTERNET" ;
+  [6]	= "WRITE_EXTERNAL_STORAGE" ;
+  } ;
+template="tool"
